651c2f05edf1dd216e07868545b8c99e7a497b5f
poojadaharwal <46249649+poojadaharwal@users.noreply.github.com>
2020-01-10 19:37:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 render(
   <BrowserRouter>
