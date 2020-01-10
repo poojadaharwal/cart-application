2eaeb99b7e4c9cda60e55e0d0fcfaf8c059da2d5
poojadaharwal <46249649+poojadaharwal@users.noreply.github.com>
2020-01-10 19:24:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port, () => console.log('Server is running at port:' + port));
