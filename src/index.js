d1950717468beec88c6eb531f44bd1ab559ad971
poojadaharwal <46249649+poojadaharwal@users.noreply.github.com>
2020-01-13 00:30:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 render(
